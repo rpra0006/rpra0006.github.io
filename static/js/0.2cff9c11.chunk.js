633790e167728f8d2a40a31d16123bbd02096d0c
Researchify <researchify@bot.com>
2021-10-06 12:48:01 +0700

    Deployed with Researchify :)

---
+++ static/js/0.2cff9c11.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbase=this.webpackJsonpbase||[]).push([[0],{107:function(s,p,a){}}]);
+//# sourceMappingURL=0.2cff9c11.chunk.js.map
---
+++ static/js/2.5a241007.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbase=this.webpackJsonpbase||[]).push([[2],{108:function(s,p,a){}}]);
+//# sourceMappingURL=2.5a241007.chunk.js.map
