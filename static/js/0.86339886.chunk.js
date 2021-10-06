82796480bf84c829f5979aba81960f53e3f18114
Researchify <researchify@bot.com>
2021-10-06 13:05:11 +0700

    Deployed with Researchify :)

---
+++ static/js/0.86339886.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbase=this.webpackJsonpbase||[]).push([[0],{106:function(s,p,a){}}]);
+//# sourceMappingURL=0.86339886.chunk.js.map
---
+++ static/js/2.b84677e1.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbase=this.webpackJsonpbase||[]).push([[2],{107:function(s,p,a){}}]);
+//# sourceMappingURL=2.b84677e1.chunk.js.map
